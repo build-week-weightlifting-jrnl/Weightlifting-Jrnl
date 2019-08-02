067607b839fbd3a3004ba9d38783f0e3202f799a
Kadeem140 <Beckford.kadeem@yahoo.com>
2019-08-02 19:54:17 -0400

    Finshed up touches to Build week project site.

---
+++ js/index.js
@@ -0,0 +1,16 @@
+let remember2 = document.querySelector(".forgot");
+
+remember2.addEventListener('click', (event) => {
+	alert('Try to remember you Log in Info please!');
+});
+
+let newContact = document.querySelector('.contact');
+
+newContact.addEventListener('mouseenter', (event) => {
+	event.target.style.backgroundColor = 'orange';
+}); 
+
+let newContact2 = document.querySelector('.contact');
+newContact2.addEventListener('mouseleave', (event) => {
+	event.target.style.backgroundColor = 'red';
+}); 
