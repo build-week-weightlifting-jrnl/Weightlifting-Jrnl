c8bb5e33c13926ec66d0f23c648a50267a579c26
Leon Chisum <leonchisum3306@gmail.com>
2019-08-05 20:22:38 -0400

    updated MVP

--- js/contact.js
+++ js/contact.js
@@ -1,9 +1,3 @@
-let remember2 = document.querySelector(".forgot");
-
-remember2.addEventListener('click', (event) => {
-	alert('Try to remember you Log in Info please!');
-});
-
 let newContact = document.querySelector('.contact');
 
 newContact.addEventListener('mouseenter', (event) => {
@@ -14,4 +8,3 @@ let newContact2 = document.querySelector('.contact');
 newContact2.addEventListener('mouseleave', (event) => {
 	event.currentTarget.style.backgroundColor = 'red';
 }); 
-
